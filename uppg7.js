ae5ebe999e985808e7db2d05dfd99089ae37db16
Erika.Lundblad95@gmail.com <Erika.Lundblad95@gmail.com>
2025-05-09 22:38:36 +0200

    videon som inte ville hänga med innan.

--- uppg7.js
+++ uppg7.js
@@ -1,7 +1,7 @@
 
 
 function uppg7() {
-   
+    
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
